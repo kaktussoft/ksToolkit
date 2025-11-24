4c46e3f1d719914e8b994c6167824e25d0d9c170
Ron Clarijs <ron.clarijs@gmail.com>
2025-11-24 21:11:15 +0100

    Add files via upload

--- example/waagje/php/functions.php
+++ example/waagje/php/functions.php
@@ -2,7 +2,7 @@
 require_once "/var/www/config/kaktussoft.nl/config_waagje.php";
 function connectToDatabase()
 {
-    $dbh = new PDO("mysql:host=localhost;charset=utf8mb4;dbname=" . DBNAME, DBUSER, DBPASSWORD, [PDO::MYSQL_ATTR_MULTI_STATEMENTS => false]);
+    $dbh = new PDO("mysql:host=localhost;charset=utf8mb4;dbname=" . DBNAME, DBUSER, DBPASSWORD, [\Pdo\Mysql::ATTR_MULTI_STATEMENTS => false]);
     $dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
     $dbh->setAttribute(PDO::ATTR_STATEMENT_CLASS, ['MyPDOStatement']);
     $dbh->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_TO_STRING);
@@ -25,5 +25,4 @@ function sanitizeListMysql($list, $PDOcon)
     $values = explode(',', $list);
     $sanitizedValues = array_map([$PDOcon, 'quote'], $values);
     return implode(',', $sanitizedValues);
-
 }
